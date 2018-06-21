d9596cdd44273a9413eda395ced07b34b589cc3d
wangshy31 <450681821@qq.com>
2018-06-22 00:29:20 +0800

    dfs and bfs

--- chusai_3.py
+++ chusai_3.py
@@ -9,10 +9,11 @@ base = 0
 mod_num = 1000000007
 
 class Node:
-	def __init__(self, index, to, cost):
-		self.index = index
+	def __init__(self, to, cost, num):
 		self.to = to
 		self.cost = cost
+		self.num = num
+
 
 paths = {}
 for line in sys.stdin:
@@ -35,6 +36,38 @@ for line in sys.stdin:
 			tmp.append(int(item))
 		ques.append(tmp)
 	count = count + 1
-print base
-print paths
+
+
+def dfs(paths, start, target):
+    if start == target:
+        return [start]
+    stack = [(start, [start])]
+    while stack:
+        (node, path) = stack.pop()
+        for next_id in range(len(paths[node])):
+            next_node = paths[node][next_id]
+            to = next_node.to
+            if to not in set(path):
+                if to == target:
+                    return path + [to]
+                else:
+                    stack.append((to, path + [to]))
+
+
+
+for i in range(m):
+    base_cost = base
+    path = dfs(paths, ques[i][0], ques[i][1])
+    if len(path) == 1:
+        print base_cost
+    else:
+        for j in range(1, len(path)):
+            nodes = paths[path[j-1]]
+            for node in nodes:
+                if node.to == path[j]:
+                    if node.num % 2 == 0:
+                        base_cost = base_cost + node.cost % mod_num
+                    else:
+                        base_cost = base_cost + mod_num - node.cost % mod_num
+        print base_cost % mod_num
 
---
+++ template.py
@@ -0,0 +1,52 @@
+#include <iomanip>
+cout<<sflags(ios::fixed)<<setprecision(2)<<result<<endl;
+
+DFS https://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
+def dfs(paths, start):
+    visited = set()
+    stack = [start]
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.add(node)
+            stack.extend(paths[node] - visited)
+    return visited
+
+def dfs(paths, start, target):
+    if start == target:
+        return [start]
+    stack = [(start, [start])]
+    while stack:
+        (node, path) = stack.pop()
+        for next_id in range(len(paths[node])):
+            next_node = paths[node][next_id]
+            to = next_node.to
+            if to not in set(path):
+                if to == target:
+                    return path + [to]
+                else:
+                    stack.append((to, path + [to]))
+
+def bfs(paths, start):
+    visited = set()
+    stack = [start]
+    while stack:
+        node = stack.pop(0)
+        if node not in visited:
+            visited.add(node)
+            stack.extend(paths[node] - visited)
+    return visited
+
+def bfs(paths, start, target):
+    if start == target:
+        return start
+    else:
+        stack = [(start, [start])]
+        while stack:
+            (node, path) = stack.pop(0)
+            for next_node in paths[node] - set(path):
+                if next_node == target:
+                    return path + next_node
+                else:
+                    stack.append((next_node, path + [next_node]))
+
